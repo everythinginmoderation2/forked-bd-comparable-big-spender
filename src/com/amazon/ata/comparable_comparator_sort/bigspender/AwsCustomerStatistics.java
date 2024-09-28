0e8207b046014f1b01bc5db7258521d9886c4936
Prince Slater <everythingindmoderation2@gmail.com>
2024-09-27 21:36:46 -0400

    getTopServiceSpendForEachCustomer done

--- src/com/amazon/ata/comparable_comparator_sort/bigspender/AwsCustomerStatistics.java
+++ src/com/amazon/ata/comparable_comparator_sort/bigspender/AwsCustomerStatistics.java
@@ -7,6 +7,7 @@ import com.amazon.ata.comparable_comparator_sort.bigspender.types.CustomerTotalS
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -33,47 +34,9 @@ public class AwsCustomerStatistics {
      */
     public List<CustomerServiceSpend> getTopServiceSpendForEachCustomer() {
         // PARTICIPANTS: Implement according to Javadoc and README
-        List<CustomerServiceSpend> newOrderedList = new ArrayList<>();
-        List<CustomerServiceSpend> exisistingUnorderedList = awsServiceInvoiceDao.getHighestServiceSpendsForEachCustomer();
-        if (exisistingUnorderedList == null) return newOrderedList;
-        if (exisistingUnorderedList.size() == 1) newOrderedList.add(exisistingUnorderedList.get(0));
-        if (exisistingUnorderedList.size() == 2) {
-            CustomerServiceSpend firstSpend = exisistingUnorderedList.get(0);
-            CustomerServiceSpend lastSpend = exisistingUnorderedList.get(1);
-            Customer firstSpendCustomer = firstSpend.getCustomer();
-            Customer lastSpendCustomer = lastSpend.getCustomer();
-            if (firstSpendCustomer.compareTo(lastSpendCustomer) == 0) {
-                newOrderedList.addAll(exisistingUnorderedList);
-                return newOrderedList;
-            } else if (firstSpendCustomer.compareTo(lastSpendCustomer) > 0) {
-                newOrderedList.add(lastSpend);
-                newOrderedList.add(firstSpend);
-                return newOrderedList;
-            } else if (firstSpendCustomer.compareTo(lastSpendCustomer) < 0) {
-                newOrderedList.add(firstSpend);
-                newOrderedList.add(lastSpend);
-                return newOrderedList;
-            }
-        }
-        for (int i = 0; i < exisistingUnorderedList.size(); i++) {
-            CustomerServiceSpend currentSpend = exisistingUnorderedList.get(i);
-            CustomerServiceSpend nextSpend = exisistingUnorderedList.get(i + 1);
-            Customer currentSpendCustomer = currentSpend.getCustomer();
-            Customer nextSpendCustomer = nextSpend.getCustomer();
-            if (currentSpendCustomer.compareTo(nextSpendCustomer) == 0) {
-                newOrderedList.addAll(exisistingUnorderedList);
-                return newOrderedList;
-            } else if (currentSpendCustomer.compareTo(nextSpendCustomer) > 0) {
-                newOrderedList.add(nextSpend);
-                newOrderedList.add(currentSpend);
-                return newOrderedList;
-            } else if (currentSpendCustomer.compareTo(nextSpendCustomer) < 0) {
-                newOrderedList.add(currentSpend);
-                newOrderedList.add(nextSpend);
-                return newOrderedList;
-            }
-        }
-        return newOrderedList;
+        List<CustomerServiceSpend> highestSpendsPerCustomer = this.awsServiceInvoiceDao.getHighestServiceSpendsForEachCustomer();
+        Collections.sort(highestSpendsPerCustomer);
+        return highestSpendsPerCustomer;
     }
 
     /**
